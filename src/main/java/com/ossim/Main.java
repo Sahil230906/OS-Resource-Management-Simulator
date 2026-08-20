4e3cb48b1f885b60791febe08e0fbac861c7bb49
Sahil <sahilj2309@gmail.com>
2026-08-20 13:14:17 +0530

    feat: initial Maven+JavaFX project setup with working window

---
+++ src/main/java/com/ossim/Main.java
@@ -0,0 +1,25 @@
+package com.ossim;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("OS Resource Management & Algorithm Simulator");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 800, 500);
+
+        primaryStage.setTitle("OS Simulator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
